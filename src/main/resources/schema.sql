aea112e2028fb74541734a902750f93fdaf756d1
FH413 <forgottenman983@gmail.com>
2024-05-04 15:39:36 +0300

    Создание заготовки для проекта

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS PHONES;
+
+CREATE TABLE PHONES (
+                               id INT PRIMARY KEY,
+                               phone_name VARCHAR(50) NOT NULL,
+                               phone_value INT NOT NULL
+
+);
+
