2a3bc246735a6c16ee1ad250d14841d525bf6b4c
FH413 <forgottenman983@gmail.com>
2024-05-07 07:41:45 +0300

    контроллер для магазина

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,16 +3,11 @@ DROP TABLE IF EXISTS phones;
 CREATE TABLE phones (
                                id INT PRIMARY KEY,
                                phone_name VARCHAR(50) NOT NULL,
-                               phone_price INT NOT NULL
+                               phone_price INT NOT NULL,
+                               phone_quantity INT NOT NULL
 
 );
-CREATE TABLE goodsinstock (
-                        id INT PRIMARY KEY,
-                        phone_id INT NOT NULL,
-                        phone_quantity INT NOT NULL,
-                        foreign key (phone_id) references phones(id)
 
-);
 
 CREATE TABLE purchase (
                               id INT PRIMARY KEY,
